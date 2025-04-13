a6f774dc78ebb7073ead4837ca2a6b47553e0040
Christian Onyeukwu <christianonyeukwu.com>
2025-04-13 15:07:56 -0400

    added link to featyred projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,23 +5,23 @@ const projects = [
     {
         id: 1,
         image: "/images/qikpharma.png",
-        title: "Real-Time Validation System",
-        description: "A robust system designed for algebraic and logical validation of real-time software systems.",
+        title: "QikPharma",
+        description: "A robust system designed for medication prescription management and consultation.",
         link: "https://qikpharma.com/"
     },
     {
         id: 2,
         image: "/images/nuodal.png",
-        title: "E-commerce Platform",
-        description: "A scalable full-stack e-commerce application built with Java, React, and Spring Boot.",
+        title: "Nuodal",
+        description: "A scalable Islamic prayer scheduler and admistration.",
         link: "#"
     },
     {
         id: 3,
         image: "/images/tindog.png",
-        title: "University Management System",
-        description: "An intuitive platform for managing university academic activities, leveraging modern web technologies.",
-        link: "#"
+        title: "TinDog",
+        description: "An intuitive platform for managing Pets especially dogs for pet lovers.",
+        link: "https://christianribig5.github.io/tindog/"
     }
 ];
 export default projects;
