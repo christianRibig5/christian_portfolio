6c67e8ca2c1c80889779b087da768362fb81cd96
Christian Onyeukwu <christianonyeukwu.com>
2025-04-13 17:48:19 -0400

    justified testimonial text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,16 @@ const About = () => {
 
                 <h2 className="text-center">About Me</h2>
                 <p className="text-justify">
-                    I'm a backend engineer specializing in Java and modern software development. While I focus on building scalable backend systems, I also support frontend projects using React and JavaScript. I hold a Master’s in Computer Science (Research Stream) from a Canadian university, where I explored algebraic and logical validation models for real-time systems.
+                    I'm a backend engineer specializing in Java and modern software development.
+                    While I focus on building scalable backend systems, I also support frontend
+                    projects using React and JavaScript. I hold a Master’s in Computer Science
+                    (thesis Stream) from a Canadian university, where I explored algebraic and
+                    logical validation models for real-time systems.
                 </p>
                 <p className="text-justify">
-                    My experience includes working as a software engineer at ECR and lecturing in the Department of Computer Science. I'm passionate about emerging tech, research, and translating complex ideas into meaningful digital solutions.
+                    My experience includes working as a software engineer at ECR and lecturing in
+                    the Department of Computer Science. I'm passionate about emerging tech, research,
+                    and translating complex ideas into meaningful digital solutions.
                 </p>
             </div>
         </section >
