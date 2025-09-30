90f4f12904c5fa2b2a7f6dc4fda57423b667b5dd
Christian Onyeukwu <christianonyeukwu.com>
2025-09-30 04:01:46 -0400

    updated the featured projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,32 +4,49 @@ import React from "react";
 const projects = [
     {
         id: 1,
+        image: "/images/solution-design.png",
+        title: "Solution Design",
+        description: `Implemented a CI/CD pipeline for a React (Vite) portfolio 
+        application using Docker, Jenkins, Kubernetes, and Ansible.`,
+        link: "/images/pipeline-log.pdf"
+    },
+    {
+        id: 2,
+        image: "/images/integration-arch.png",
+        title: "Integration Architecture",
+        description: `Implemented a CI/CD pipeline for a React (Vite) portfolio 
+        application using Docker, Jenkins, Kubernetes, and Ansible.`,
+        link: "/images/pipeline-log.pdf"
+    },
+    {
+        id: 3,
+        image: "/images/pipeline1.png",
+        title: "Implementation on AWS Amplify and EC2",
+        description: `Implemented a CI/CD pipeline for a React (Vite) portfolio 
+        application using Docker, Jenkins, Kubernetes, and Ansible.`,
+        link: "/images/pipeline-log.pdf"
+    },
+    {
+        id: 4,
         image: "/images/qikpharma.png",
         title: "QikPharma",
         description: "A robust system designed for medication prescription management and consultation.",
         link: "https://qikpharma.com/"
     },
     {
-        id: 2,
+        id: 5,
         image: "/images/nuodal.png",
         title: "Nuodal",
         description: "A scalable Islamic prayer scheduler and admistration.",
         link: "#"
     },
     {
-        id: 3,
+        id: 6,
         image: "/images/tindog.png",
         title: "TinDog",
         description: "An intuitive platform for managing Pets especially dogs for pet lovers.",
         link: "https://christianribig5.github.io/tindog/"
     },
-    {
-        id: 4,
-        image: "/images/pipeline1.png",
-        title: "CI/CD Workflow on AWS Amplify and EC2",
-        description: `Implemented a CI/CD pipeline for a React (Vite) portfolio 
-        application using Docker, Jenkins, Kubernetes, and Ansible.`,
-        link: "/images/pipeline-log.pdf"
-    }
+
 ];
 export default projects;
